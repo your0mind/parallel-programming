22273c5134135aafdcaf34f83454131dea53096e
Kiselev Denis <denis.kiselev.1997@gmail.com>
2018-12-03 19:47:24 +0300

    lab started

---
+++ 1608/kiselev_ds/task3/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#include "mpi.h"
+
+int compare_digits(int num1, int num2, int digit_num) {
+	
+}
+
+int main(int argc, char *argv[]) {
+	int proc_num, proc_rank;
+
+	/*MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &proc_num);
+	MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
+
+	MPI_Finalize();*/
+	return 0;
+}
